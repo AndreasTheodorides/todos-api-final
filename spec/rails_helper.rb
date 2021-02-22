cf518a169a3d54320b917c7723f97b86b7b47eb7
Andreas Theodoridis <antreastheodorides16@gmail.com>
2021-02-22 11:55:36 +0200

    generate controllers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -60,3 +60,11 @@ RSpec.configure do |config|
     end
   end
 end
+# [...]
+Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+# [...]
+RSpec.configuration do |config|
+  # [...]
+  config.include RequestSpecHelper, type: :request
+  # [...]
+end
